953453600674e0077ae5ba92e194b1e66ba387b2
www-bory72@yandex.ru <www-bory72@yandex.ru>
2020-10-03 15:42:58 +0300

    Добавьте файлы проекта.

---
+++ DamageBoard/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamageBoard
+{
+    class Calculator
+    {
+        public double str;
+        public double wStr;
+        public double dMod;
+        public double damage1;
+        public double damage2;
+
+        public Calculator(double str, double wStr, double dMod)
+        {
+            this.str = str;
+            this.wStr = wStr;
+            this.dMod = dMod;
+        }
+
+        public string Cacl()
+        {
+            damage1 = (str * 0.82 + wStr) * dMod;
+            damage2 = damage1 + wStr;
+            string res = Math.Round(damage1, 2) + "--" + Math.Round(damage2, 2) + " hpt";
+            return res;
+        }
+    }
+}
---
+++ DamageBoard/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DamageBoard
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.IsEnabled = true;
+            timer.Tick += (o, t) => { timie.Content = DateTime.Now.ToString(); };
+            timer.Start();
+        }
+
+        private void run_Click(object sender, RoutedEventArgs e)
+        {
+            double str = double.Parse(strength.Text);
+            double wStr = double.Parse(Wstrength.Text);
+            double dMod = double.Parse(dModifier.Text);
+            Calculator calculator = new Calculator(str, wStr, dMod);
+            resu.Text=calculator.Cacl();
+            DateTime dt = new DateTime();
+            date.Text = DateTime.Now.ToString();
+        }
+
+        private void strength_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+        }
+
+        
+
+        private void Wstrength_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void dModifier_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void result_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
